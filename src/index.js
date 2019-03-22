f06ed73d5c4a5dea1d3159ffb750010624b6c7c6
Ramiz Altynbaev <ramiz@quinncia.io>
2019-03-22 13:58:32 -0300

    Change middleware to be exported by default

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Model from './lib/model';
 import Connector from './lib/connector';
-import { middleware } from './lib/middleware';
+import middleware from './lib/middleware';
 
 export {
   Model,
